5d12d1af3b7940d1a3c47a35b9bcca9231ca5767
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-06-30 12:30:08 +0530

    section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
+import sec from "../../assets/data/extraSectionData.js";
 import bgImage from "../../assets/images/bg-image.jpg";
+import { ExtraSections } from "../../components/HomePage/ExtraSections/ExtraSections";
 import { Hero } from "../../components/HomePage/Hero/Hero";
 import { Navbar } from "../../components/HomePage/Navbar/Navbar";
 import "./Home.scss";
@@ -18,13 +20,25 @@ export const Home = () => {
           <Hero />
         </div>
       </header>
-      {/* <main>
+      <main>
+        {sec.map((data, index) => {
+          return (
+            <ExtraSections
+              key={index}
+              heading={data.heading}
+              para={data.para}
+              order_first={data.order_first}
+              order_second={data.order_second}
+              image={data.image}
+              imageAlt={data.imageAlt}
+            />
+          );
+        })}
 
+        {/* <ExtraSections />
+        <ExtraSections />
+        <ExtraSections /> */}
       </main>
-      <footer>
-
-      </footer>
-      <h1>Home</h1> */}
     </div>
   );
 };
