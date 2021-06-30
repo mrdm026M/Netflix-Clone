12f4aaa011d2b41ea6db5a4d10737b45c880129a
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-06-30 12:29:44 +0530

    section data added

---
+++ src/assets/data/extraSectionData.js
@@ -0,0 +1,41 @@
+import two from "../images/2.jpg";
+
+const sec = [
+  {
+    heading: "Enjoy on your TV.",
+    para:
+      "Watch on smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+    order_first: 1,
+    order_second: 2,
+    image: two,
+    imageAlt: "Extra",
+  },
+  {
+    heading: "Download your shows to watch offline.",
+    para: "Save your favourites easily and always have something to watch.",
+    order_first: 2,
+    order_second: 1,
+    image: two,
+    imageAlt: "Extra",
+  },
+  {
+    heading: "Watch everywhere.",
+    para:
+      "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
+    order_first: 1,
+    order_second: 2,
+    image: two,
+    imageAlt: "Extra",
+  },
+  {
+    heading: "Create profiles for children.",
+    para:
+      "Send children on adventures with their favourite characters in a space made just for them--free with your membership.",
+    order_first: 2,
+    order_second: 1,
+    image: two,
+    imageAlt: "Extra",
+  },
+];
+
+export default sec;
