9e7efbf243e443e624c3bcffc8096e42e06d37d9
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-07-08 12:59:44 +0530

    accordian data added

---
+++ src/assets/data/accordianData.js
@@ -0,0 +1,34 @@
+const accData = [
+  {
+    question: "What is Netflix?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto architecto nam nulla esse voluptatem dolores eligendi iste quo cupiditate numquam?",
+  },
+  {
+    question: "How much does Netflix cost?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto architecto nam nulla esse voluptatem dolores eligendi iste quo cupiditate numquam?",
+  },
+  {
+    question: "Where can I watch?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto architecto nam nulla esse voluptatem dolores eligendi iste quo cupiditate numquam?",
+  },
+  {
+    question: "How do I cancel?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto architecto nam nulla esse voluptatem dolores eligendi iste quo cupiditate numquam?",
+  },
+  {
+    question: "What can I watch on Netflix?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto architecto nam nulla esse voluptatem dolores eligendi iste quo cupiditate numquam?",
+  },
+  {
+    question: "Is Netflix good for kids?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto architecto nam nulla esse voluptatem dolores eligendi iste quo cupiditate numquam?",
+  },
+];
+
+export default accData;
