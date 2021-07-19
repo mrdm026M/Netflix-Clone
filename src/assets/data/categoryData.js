5454136aebff0d0f82e24c196110ed2c8dad8674
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-07-19 13:49:04 +0530

    category data added

---
+++ src/assets/data/categoryData.js
@@ -0,0 +1,38 @@
+import cimage from "../images/2.jpg";
+
+const categoryData = [
+  {
+    img: cimage,
+    name: "Action & Adventure",
+  },
+  {
+    img: cimage,
+    name: "Amine",
+  },
+  {
+    img: cimage,
+    name: "Thriller",
+  },
+  {
+    img: cimage,
+    name: "Sci-Fi & Fantasy",
+  },
+  {
+    img: cimage,
+    name: "Dramas",
+  },
+  {
+    img: cimage,
+    name: "Crime",
+  },
+  {
+    img: cimage,
+    name: "Horror",
+  },
+  {
+    img: cimage,
+    name: "History",
+  },
+];
+
+export default categoryData;
