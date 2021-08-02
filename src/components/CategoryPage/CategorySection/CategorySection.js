954ac4b42f59c4a667f87758c2c060147d679d73
mrdm026M <mrdm026@gmail.com>
2021-08-02 12:52:56 +0530

    category section updated

--- src/components/CategoryPage/CategorySection/CategorySection.js
+++ src/components/CategoryPage/CategorySection/CategorySection.js
@@ -1,12 +1,22 @@
 import React from "react";
 import category from "../../../assets/data/categoryData";
-import { Categories } from "../../StyledComponent/Categories/Categories";
+import "./CategorySection.scss";
 
 export const CategorySection = () => {
   return (
     <div className="category__section__content">
       {category.map((categories) => {
-        return <Categories img={categories.img} name={categories.name} />;
+        return (
+          <div
+            className="single__category"
+            style={{
+              backgroundImage: `url(${categories.img})`,
+              marginTop: `${categories.margin}`,
+            }}
+          >
+            <span>{categories.name}</span>
+          </div>
+        );
       })}
     </div>
   );
