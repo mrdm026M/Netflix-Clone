28fd79d9059e633da0c46018781c8ea22c37e3c3
mrdm026M <mrdm026@gmail.com>
2021-08-02 12:52:34 +0530

    margin data added

--- src/assets/data/categoryData.js
+++ src/assets/data/categoryData.js
@@ -4,34 +4,42 @@ const categoryData = [
   {
     img: cimage,
     name: "Action & Adventure",
+    margin: "0.5rem",
   },
   {
     img: cimage,
     name: "Amine",
+    margin: "3.5rem",
   },
   {
     img: cimage,
     name: "Thriller",
+    margin: "2rem",
   },
   {
     img: cimage,
     name: "Sci-Fi & Fantasy",
+    margin: "0.5rem",
   },
   {
     img: cimage,
     name: "Dramas",
+    margin: "1.5rem",
   },
   {
     img: cimage,
     name: "Crime",
+    margin: "3.5rem",
   },
   {
     img: cimage,
     name: "Horror",
+    margin: "1rem",
   },
   {
     img: cimage,
     name: "History",
+    margin: "0rem",
   },
 ];
 
