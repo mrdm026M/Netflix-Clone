1e00bf08a59bda6bce603846c56a579a00a43d85
mrdm026M <mrdm026@gmail.com>
2021-08-04 02:11:05 +0530

    assets added

--- src/assets/data/extraSectionData.js
+++ src/assets/data/extraSectionData.js
@@ -1,13 +1,15 @@
-import two from "../images/2.jpg";
+import one from "../images/1.png";
+import two from "../images/2.png";
+import three from "../images/3.png";
+import four from "../images/4.png";
 
 const sec = [
   {
     heading: "Enjoy on your TV.",
-    para:
-      "Watch on smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+    para: "Watch on smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
     order_first: 1,
     order_second: 2,
-    image: two,
+    image: one,
     imageAlt: "Extra",
   },
   {
@@ -20,20 +22,18 @@ const sec = [
   },
   {
     heading: "Watch everywhere.",
-    para:
-      "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
+    para: "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
     order_first: 1,
     order_second: 2,
-    image: two,
+    image: three,
     imageAlt: "Extra",
   },
   {
     heading: "Create profiles for children.",
-    para:
-      "Send children on adventures with their favourite characters in a space made just for them--free with your membership.",
+    para: "Send children on adventures with their favourite characters in a space made just for them--free with your membership.",
     order_first: 2,
     order_second: 1,
-    image: two,
+    image: four,
     imageAlt: "Extra",
   },
 ];
