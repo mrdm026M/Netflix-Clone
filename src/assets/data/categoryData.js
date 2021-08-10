c920e265bbc7d48cb17d86b25090cffae794b75f
mrdm026M <mrdm026@gmail.com>
2021-08-10 13:48:16 +0530

    img added

--- src/assets/data/categoryData.js
+++ src/assets/data/categoryData.js
@@ -1,50 +1,57 @@
-import cimage from "../images/2.jpg";
+import action from "../images/moviesGenrePoster/action.jpg";
+import adventure from "../images/moviesGenrePoster/adventure.jpg";
+import crime from "../images/moviesGenrePoster/crime.jpg";
+import dramas from "../images/moviesGenrePoster/dramas.jpg";
+import history from "../images/moviesGenrePoster/history.jpg";
+import horror from "../images/moviesGenrePoster/horror.jpg";
+import scifi from "../images/moviesGenrePoster/sci-fi.jpg";
+import thriller from "../images/moviesGenrePoster/thriller.jpg";
 
 const categoryData = [
   {
-    img: cimage,
+    img: action,
     name: "Action",
     margin: "0.5rem",
     id: "28",
   },
   {
-    img: cimage,
+    img: adventure,
     name: "Adventure",
     margin: "3.5rem",
     id: "12",
   },
   {
-    img: cimage,
+    img: thriller,
     name: "Thriller",
     margin: "2rem",
     id: "53",
   },
   {
-    img: cimage,
+    img: scifi,
     name: "Science Fiction",
     margin: "0.5rem",
     id: "878",
   },
   {
-    img: cimage,
+    img: dramas,
     name: "Dramas",
     margin: "1.5rem",
     id: "18",
   },
   {
-    img: cimage,
+    img: crime,
     name: "Crime",
     margin: "3.5rem",
     id: "80",
   },
   {
-    img: cimage,
+    img: horror,
     name: "Horror",
     margin: "1rem",
     id: "27",
   },
   {
-    img: cimage,
+    img: history,
     name: "History",
     margin: "0rem",
     id: "36",
