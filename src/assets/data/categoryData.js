1a7e1b0385be857e168f2a759195dc2a70941010
mrdm026M <mrdm026@gmail.com>
2021-08-10 13:31:06 +0530

    id added

--- src/assets/data/categoryData.js
+++ src/assets/data/categoryData.js
@@ -3,43 +3,51 @@ import cimage from "../images/2.jpg";
 const categoryData = [
   {
     img: cimage,
-    name: "Action & Adventure",
+    name: "Action",
     margin: "0.5rem",
+    id: "28",
   },
   {
     img: cimage,
-    name: "Amine",
+    name: "Adventure",
     margin: "3.5rem",
+    id: "12",
   },
   {
     img: cimage,
     name: "Thriller",
     margin: "2rem",
+    id: "53",
   },
   {
     img: cimage,
-    name: "Sci-Fi & Fantasy",
+    name: "Science Fiction",
     margin: "0.5rem",
+    id: "878",
   },
   {
     img: cimage,
     name: "Dramas",
     margin: "1.5rem",
+    id: "18",
   },
   {
     img: cimage,
     name: "Crime",
     margin: "3.5rem",
+    id: "80",
   },
   {
     img: cimage,
     name: "Horror",
     margin: "1rem",
+    id: "27",
   },
   {
     img: cimage,
     name: "History",
     margin: "0rem",
+    id: "36",
   },
 ];
 
